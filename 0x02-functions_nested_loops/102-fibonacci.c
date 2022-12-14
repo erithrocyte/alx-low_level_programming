4cb124d3d1d4a7aeab3a84a3c91468092efc2ba9
erithrocyte <emofoluwawa@gmail.com>
2022-12-14 04:21:17 +0000

    add missing file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.>
+
+/**
+ * main - print first 50 fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int counter;
+	int counto - 50;
+	long a = 1;
+	long a= 2;
+
+	for (counter = 1; counter <= (counto / 2; counter++)
+	{
+		printf("%1i %1i, a, b);
+		1+= b;
+		b += a;
+	}
+	if (countto % 2 == 1
+		printf("%1i", a);
+	printf("\n");
+
+	return (0);
+}
