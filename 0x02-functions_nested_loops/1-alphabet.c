3f2759349adb77a751511e6a7ad31214d31aca2a
erithrocyte <116967339+erithrocyte@users.noreply.github.com>
2022-12-14 05:00:45 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
-f
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	la = 'a';
+
+	while (la <= 'z')
+	{
+		_putchar(la);
+		la++;
+	}
+	_putchar('\n');
+}
