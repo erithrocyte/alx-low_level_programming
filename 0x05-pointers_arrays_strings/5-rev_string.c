2c3ed8b134fe28af791748a6d75a59468843033f
erithrocyte <116967339+erithrocyte@users.noreply.github.com>
2022-12-21 03:40:11 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,40 +1,40 @@
 #include "holberton.h"
 
 /**
- *  * _strlen - returns the length of a string
- *   * @s: string
- *    * Return: length
- *     */
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
 
 int _strlen(char *s)
 {
-		int len = 0;
+	int len = 0;
 
-			while (*s != '\0')
-					{
-								len++;
-										s++;
-											}
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 
-				return (len);
+	return (len);
 }
 
 /**
- *  * rev_string - reverses a string
- *   * @s: string to reverse
- *    */
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
 
 void rev_string(char *s)
 {
-		int i = 0;
-			int j = (_strlen(s) - 1);
-				char tmp;
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char tmp;
 
-					while (i < j)
-							{
-										tmp = s[i];
-												s[i] = s[j];
-														s[j] = tmp;
-																i++, j--;
-																	}
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++, j--;
+	}
 }
