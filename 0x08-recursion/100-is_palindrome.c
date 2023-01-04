681ffd789bc781c904fb9c17160bcb45f7930042
erithrocyte <116967339+erithrocyte@users.noreply.github.com>
2023-01-04 22:40:48 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,14 @@
+#include "main.h"
 
+/**
+ * is_palindrome - check if palindrome
+ * @s: string to check
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen(s);
+
+	return (compare(s, (s + len - 1)));
+}
